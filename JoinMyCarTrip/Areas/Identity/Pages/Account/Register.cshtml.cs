bf0c874ab4a3737feda63f201b1bb425c72dab18
zvezdigit <zvezdelina.vasileva@gmail.com>
2022-03-11 18:06:49 +0200

    application user in LogOut added

--- JoinMyCarTrip/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ JoinMyCarTrip/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -128,7 +128,8 @@ namespace JoinMyCarTrip.Areas.Identity.Pages.Account
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
-                await _userManager.SetFullNameAsync(user, Input.FullName);
+                user.FullName = Input.FullName;
+                
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
